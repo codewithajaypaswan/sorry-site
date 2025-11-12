5c31d70f1eb90063a5b8366d9b31c861c5e775da
ajaypaswan001ap <codewithajaypaswan@gmail.com>
2025-11-12 22:49:40 +0530

    Add static build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/sorry-site',
-  assetPrefix: '/sorry-site/',
+  basePath: isProd ? '/sorry-site' : '',
+  assetPrefix: isProd ? '/sorry-site/' : '',
+  images: { unoptimized: true }, // Required for static export
+  trailingSlash: true, // Ensures correct static routing
 };
 
 export default nextConfig;
