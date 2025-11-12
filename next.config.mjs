72d5242f32543f10f2ef819adbdbdd3821c53ea0
ajaypaswan001ap <codewithajaypaswan@gmail.com>
2025-11-12 22:01:36 +0530

    push the changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/sorry-site',
+  assetPrefix: '/sorry-site/',
+};
 
 export default nextConfig;
